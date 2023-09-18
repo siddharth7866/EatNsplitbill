8837a529161f0f0d685dc2b16fb2f0bd8efe823a
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-09-18 15:02:30 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App() {
   return (
-    <div>
-      <Hello />
+    <div className="app">
+      <div className="sidebar">
+        <FireindsList />
+      </div>
     </div>
   );
 }
 
-function Hello() {
+function FireindsList() {
+  const friends = initialFriends;
   return (
-    <div>
-      <h1>Hello React World</h1>
-    </div>
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}
+        </p>
+      )}
+    </li>
   );
 }
